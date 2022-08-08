199256612048f71a96e311159fb5bf6d6342b25e
Ashadul Mridha <ashadulmridhaprog@gmail.com>
2022-08-08 16:27:56 +0600

    law plan added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import HeroSection from "../components/HeroSection";
+import LawPlan from "../components/LawPlan";
 
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <LawPlan />
     </>
   )
 }
